ef95b99a243719123b6bbea34e2633a0cecf6824
S <S@LENOVO-S145>
2022-11-15 13:16:59 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class HelloWorld {
+    public static int Main(string[] args)
+    {
+        int a = 0;
+        if (a == 0)
+        {
+            Console.WriteLine($"Добро пожаловать в демонстрирующую базовые примеры программирования на с#");
+            Console.WriteLine($"Введите любое число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            while (b != 0)
+            {
+                b = b * 0;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Аргумент не равен 0");
+        }
+        return 1;
+    }
+}
